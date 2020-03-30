ccf7103492cb20fa859e73a8bfb28699027c18a2
dongju93 <spdlqj011@gmail.com>
2020-03-31 04:23:18 +0900

    1. card styling

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  TextInput,
+  Dimensions,
+  Platform
+} from 'react-native';
+
+// 휴대폰의 화면 크기를 구하는 정의
+const { height, width } = Dimensions.get("window")
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar barStyle="light-content" />
+      <Text style={styles.title}>Native TO DO</Text>
+      <View style={styles.card}>
+        <TextInput style={styles.input} placeholder={"New TO DO"} />
+      </View>
     </View>
   );
 }
@@ -12,8 +27,40 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f23657',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  title: {
+    color: "white",
+    fontSize: 30,
+    marginTop: 50,
+    fontWeight: "300",
+    marginBottom: 30
+  },
+  card: {
+    backgroundColor: "white",
+    flex: 1,
+    // Dimensions 함수로 구한 휴대폰의 width 에서 -25 크기의 card
+    width: width - 30,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    // iOS와 안드로이드 shadow 설정을 각자 다르게 주는 방법
+    ...Platform.select({
+      ios: {
+        shadowColor: "rgb(50, 50, 50)",
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        shadowOffset: {
+          height: -1,
+          width: 0
+        }
+      },
+      android: {
+        elevation: 3,
+      }
+    })
+  },
+  input: {
+    color: "black"
+  }
 });
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# rn-todo
-React Native로 TODO 앱 만들기
+# TO DO LIST - React Native
+
+EXPO를 활용한 React Native로 TO DO 앱 만들기
+[구상중인 프로젝트 연습용]
